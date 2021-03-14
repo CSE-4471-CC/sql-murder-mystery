44ce83b4feb598a2a9eb9e45585bdde1bbf523b5
tchmura84 <80491796+tchmura84@users.noreply.github.com>
2021-03-14 15:36:15 -0400

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,59 @@
+--
+-- File generated with SQLiteStudio v3.2.1 on Sun Mar 14 15:33:56 2021
+--
+-- Text encoding used: System
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Table: BUILDING_ACCESS
+CREATE TABLE BUILDING_ACCESS(
+Building_ID INT NOT NULL,
+Building_date DATE NOT NULL,
+Building_time TIME NOT NULL,
+User_ID INT NOT NULL,
+PRIMARY KEY(Building_ID)
+FOREIGN KEY(User_ID) references USERS(User_ID));
+
+-- Table: COMPUTER_ACCESS
+CREATE TABLE COMPUTER_ACCESS(
+Terminal_ID INT NOT NULL,
+Computer_date DATE,
+Computer_time TIME,
+User_ID INT NOT NULL,
+PRIMARY KEY(Terminal_ID)
+FOREIGN KEY(User_ID) references USERS(User_ID));
+
+-- Table: COMPUTER_TERMINALS
+CREATE TABLE COMPUTER_TERMINALS(
+Terminal_ID INT NOT NULL,
+Location VARCHAR(20) NOT NULL,
+PRIMARY KEY(Terminal_ID)
+FOREIGN KEY(Terminal_ID) references COMPUTER_ACCESS(Terminal_ID));
+
+-- Table: QUESTIONNAIRE
+CREATE TABLE QUESTIONNAIRE(
+User_ID INT NOT NULL,
+Favorite_food VARCHAR(30),
+Favorite_drink VARCHAR(30),
+Favorite_hobby VARCHAR(30),
+PRIMARY KEY(User_ID)
+FOREIGN KEY(User_ID) references USERS(User_ID));
+
+-- Table: USER_INFO
+CREATE TABLE USER_INFO(
+User_ID INT NOT NULL,
+First_name VARCHAR(20) NOT NULL,
+Last_name VARCHAR(20) NOT NULL,
+Superhero_Name VARCHAR(30),
+PRIMARY KEY(User_ID)
+FOREIGN KEY(User_ID) references USERS(User_ID));
+
+-- Table: USERS
+CREATE TABLE USERS(
+User_ID INT NOT NULL,
+Password VARCHAR(30) NOT NULL,
+PRIMARY KEY(User_ID));
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
