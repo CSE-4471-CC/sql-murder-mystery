4def31ae0f186922e2445af46780f44cc11ac393
Lia Ferguson <lkferguson7@gmail.com>
2021-04-04 07:28:25 -0400

    completed trojan horse popup window.

--- sql-murder-mystery/src/trojanmodal.js
+++ sql-murder-mystery/src/trojanmodal.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
+import Spinner from 'react-bootstrap/Spinner';
 import './Style.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -13,12 +14,13 @@ class TrojanModal extends React.Component{
   constructor (props) {
     super(props);
 
-    this.state = {show: this.props.show, isQuerySuccessful: false, f_name: '', l_name: ''};
+    this.state = {show: this.props.show, submitClicked: false, isQuerySuccessful: false, f_name: '', l_name: '', nextButton: false};
 
     this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
     this.handleLastNameChange = this.handleLastNameChange.bind(this);
 		this.handleName = this.handleName.bind(this);
 		this.handleClose = this.handleClose.bind(this);
+		this.showNextButton = this.showNextButton.bind(this);
   }
 
   handleFirstNameChange(e){
@@ -35,11 +37,17 @@ class TrojanModal extends React.Component{
 		this.setState({show: false});
 	}
 
+
+	showNextButton(){
+		this.setState({nextButton: true})
+	}
+
 	async handleName(){
 		var response = await this.executeQuery(this.state.f_name, this.state.l_name);
 		var isQuerySuccessful = response.isSuccess == 'true' ? true : false
 		this.setState({isQuerySuccessful: isQuerySuccessful});
-		this.setState({isClicked: true});
+		this.setState({submitClicked: true});
+		this.props.setTimeout(this.showNextButton, 4000);
 	}
 	
 	async executeQuery(f_name, l_name){
@@ -59,6 +67,28 @@ class TrojanModal extends React.Component{
   
   render(){
 
+		let submitButton = <Button className = 'login-button' variant='primary' onClick = {this.handleName}>Submit</Button>;
+		if (this.state.submitClicked){
+			submitButton =   <Button variant="primary" >
+				<Spinner
+					as="span"
+					animation="border"
+					size="sm"
+					role="status"
+					aria-hidden="true"
+				/>
+				Loading...
+			</Button>;
+		}
+
+		let successText = '';
+		let nextButton = null;
+		if(this.state.nextButton){
+			submitButton = <Button disabled variant='primary'>Success</Button>;
+			successText = <p>Your information was received! On to the shortcut!</p>;
+			nextButton = <Button variant='primary' href='/paddedcell'>Continue</Button>;
+		}
+
     return(
 			<Container fluid="md">
 				<Modal
@@ -76,19 +106,21 @@ class TrojanModal extends React.Component{
 								quicker! Please enter your first and last name so you can gain access to the shortcut.
 							</p>
 							<Form>
-							<div align='center' className='login-form'>
-							<h3 className='sub-headers'>Unlock Shortcut</h3>
-							<Form.Group controlId='First Name'>
-								<Form.Label className='login-labels'>First Name</Form.Label>
-								<Form.Control value={this.state.f_name} placeholder="Enter your first name here" onChange={this.handleFirstNameChange}></Form.Control>
-							</Form.Group>
-							<Form.Group controlId='password'>
-								<Form.Label className='login-labels' >Last Name</Form.Label>
-								<Form.Control value= {this.state.l_name}  placeholder="Enter your last name here" onChange={this.handleLastNameChange} ></Form.Control>
-							</Form.Group>
-							<Button className = 'login-button' variant='primary' onClick = {this.handleName}>Submit</Button>
-							</div>
-						</Form> 
+								<div align='center' className='login-form'>
+									<h3 className='sub-headers'>Unlock Shortcut</h3>
+									<Form.Group controlId='First Name'>
+										<Form.Label className='login-labels'>First Name</Form.Label>
+										<Form.Control value={this.state.f_name} placeholder="Enter your first name here" onChange={this.handleFirstNameChange}></Form.Control>
+									</Form.Group>
+									<Form.Group controlId='password'>
+										<Form.Label className='login-labels' >Last Name</Form.Label>
+										<Form.Control value= {this.state.l_name}  placeholder="Enter your last name here" onChange={this.handleLastNameChange} ></Form.Control>
+									</Form.Group>
+									{submitButton}
+									{successText}
+									{nextButton}
+								</div>
+							</Form> 
 						</Modal.Body>
 					</Modal.Dialog>
 				</Modal>
