4a018cfdea198b2cc26e343f7e330ebb36ca7969
Julia Workum <32983293+juliaworkum@users.noreply.github.com>
2021-04-06 19:33:51 -0400

    Update rules.js

--- sql-murder-mystery/src/rules.js
+++ sql-murder-mystery/src/rules.js
@@ -19,8 +19,8 @@ class Rules extends React.Component {
                 <p>
                     This is an interactive game that will show you how SQL Injection can be used to exploit webpages in order to retrieve confidential information from their underlying database.
                     You will also test your knowledge of SQL Injection and other important security topics through quiz-style questions and interactive activities.
-                    Each step of the game will require you to successfully execute SQL statements or prove your knowledge about SQL Injection to uncover clues that will help you to solve the unsolved murder of Tony Stark.
-          There will be helpful hints along the way if you need help with the task at hand. <br></br> <br></br>
+                    Each step of the game will require you to successfully execute SQL statements or prove your knowledge about SQL Injection in order to uncover clues and solve the murder of Tony Stark.
+          There will be helpful hints along the way if you need assistance with the task at hand. <br></br> <br></br>
 
           As part of the game, text files may be downloaded onto your computer in order to relay important clues or information to you. They will be stored in a file called "SQL-Mystery-Game-Files" on your Desktop. <b> Good luck and happy hacking!</b>
 
