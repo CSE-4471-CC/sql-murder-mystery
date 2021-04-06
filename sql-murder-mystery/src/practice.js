39effd8bf12ef7fbb91b467f2c2e158be5514656
tchmura84 <80491796+tchmura84@users.noreply.github.com>
2021-04-06 00:50:38 -0400

    Update practice.js

--- sql-murder-mystery/src/practice.js
+++ sql-murder-mystery/src/practice.js
@@ -14,22 +14,40 @@ class Practice extends React.Component{
     return(
       <Container fluid='md'>
         <h2 className='sub-headers'>Let's Practice!</h2>
-        <p>SQL Injection most often happens when a user is prompted for input on a piece of information, such as a User ID. Instead of putting in their ID, they would input a SQL Statement that runs through the systems database and retrieves sensitive information.
-              Let's run through this process with a simple practice problem so we can get your feet wet before we embark on the real challenge! </p>
-              <p>In the form below, we're going to retrieve the password information for "John Doe". In the "Username" slot, input the following SQL Query:
-                  <b>SELECT Password FROM user_database WHERE name = 'John Doe'</b>  
-              </p>
+        <p>SQL Injection most often when a user is prompted for input on a piece of information, such as a User ID. Instead of putting in their ID, they would input a SQL Statement that runs through the systems database retrieves sensitive information.
+              Let's run through this process with a few simple practice problems so we can get your feet wet before we embark on the real challenge! </p>
+              </br>
+              <p>In the form below, we're going to retrieve the password information for "John Doe". In the "Username" slot, input the following SQL Query:</p>
+                  <b>SELECT Password FROM Users WHERE name = 'John Doe'</b>  
+                </p>
                 <form>
                   <div class="form-group">
                     <label for="username">Username</label>
-                    <input type="username" class="form-control"></input>
+                    <input type="username" class="form-control">
                   </div>
-                  <button type="button" class="btn btn-primary" onclick="sqlReveal()">Login</button>
+                  </br>
+                  <button type="button" class="btn btn-primary" onclick=sqlRevealPractice()>Login</button>
                 </form>
-                <ul class="list-group" id="sql-output-reveal">
-                  <li class="list-group-item">Password = Password123</li>
-                </ul>
-                <p> <b>Good luck with the Mystery!</b></p>
+                </br>
+                <p>SQL Batch Injection can be utilitzed to manipulate data outside of the confines of the table used in the login. Multiple SQL statements are joined together in order to retrieve sensitive information for the database. 
+                  In the example below, we're going to retrieve information related to "John Doe" that exists in the same schema as the previous example, but within a different table that would be impossible to access via the usual login page.
+                 </p>
+                  </br>
+                  <p>The first SQL statement below is used to access the schema, while we retrieve the necessary server information with the second statement. Copy the combined statement into the following login box below: </p>
+                      <b>"; SELECT Computer_Number FROM Computers WHERE Name = 'John Doe' --
+                      </b>  
+                    </p>
+                    <form>
+                      <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="username" class="form-control">
+                      </div>
+                      </br>
+                      <button type="button" class="btn btn-primary" onclick=sqlBatchRevealPractice()>Login</button>
+                    </form>
+                    </br>
+                </br>
+                <p> <b>Good luck with the Mystery!</b></p> 
         <Button variant="outline-primary float-left" href="/rules" >Back</Button>
         <Button variant="outline-primary float-right" href="/step1">Start Game!</Button>
       </Container>
